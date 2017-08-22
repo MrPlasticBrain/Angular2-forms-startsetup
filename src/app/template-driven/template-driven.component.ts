be6f68cf892a002f994d451b59cfdc4a0686a27e
mrplasticbrain <alekmilanov.sr@gmail.com>
2017-08-22 22:56:31 +0200

    forms test in angular 2

---
+++ src/app/template-driven/template-driven.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'f-template-driven',
+  templateUrl: './template-driven.component.html',
+  styles: []
+})
+export class TemplateDrivenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
